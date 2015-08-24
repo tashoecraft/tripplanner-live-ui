ec7b57248e2b1e1a821e7efa678306b049f188ee
Austin <tashoecraft@gmail.com>
2015-08-24 10:16:47 -0400

    first commit

---
+++ models/day.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var DaySchema = new mongoose.Schema({
+  number: Number,
+  hotel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel'
+  },
+  restaurants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant'
+  }],
+  activities: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Activity'
+  }]
+});
